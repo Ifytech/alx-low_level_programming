63de1906aa317dc3602220495e30fecc687c4e4c
Ifytech <simonabugu19@gmail.com>
2022-09-09 20:42:10 -0700

    Positive anything is better than negative nothing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,9 @@
 
 /**
  *
- *   main - print the string in the put function
+ *   main - print the string in the put function   
  *
- *    
- *
- *     Description: using the main function 
+      Description: using the main function 
  *
  *      this program prints "programming is like building a multilingual puzzle
  *
